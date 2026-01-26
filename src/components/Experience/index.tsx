a75d75ab504ccbb825f78f8b04393a49505d176e
TenzDelek <tibetdelek@gmail.com>
2026-01-26 12:37:16 +0530

    transform control

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,4 +1,4 @@
-import { OrbitControls } from "@react-three/drei";
+import { TransformControls, OrbitControls } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber"
 import { useRef } from "react"
 import Custom from "../Custom/Index"
@@ -7,8 +7,8 @@ const Experience = () => {
   const cubeRef = useRef<any>(null);
   useFrame((state, delta) => {
     const angle = state.clock.elapsedTime;
-    state.camera.position.x = Math.sin(angle) * 8;
-    state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
     state.camera.lookAt(0, 0, 0);
     cubeRef.current.rotation.y += delta // for such thing like animation dont use state, use ref directly
     //we use delta to make animation sync. because every person have different fps
@@ -23,6 +23,7 @@ const Experience = () => {
         <boxGeometry />
         <meshStandardMaterial color="mediumpurple" />
       </mesh>
+      <TransformControls object={cubeRef} mode="scale" /> {/* mode is rotate, translate, scale */}
       <mesh rotation-x={-Math.PI / 2} position={[0, -1, 0]} scale={10}> {/* roation to - to see the face */}
         <planeGeometry />
         <meshStandardMaterial color="greenyellow" />
